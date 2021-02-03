963ab9a0f444e6dc4b93763b9a21cdc1c285608c
DoctorProctor <45911516+1DoctorProctor1@users.noreply.github.com>
2021-02-03 02:20:03 +0100

    Roadmap :D

---
+++ script/roadmap.js
@@ -0,0 +1,26 @@
+var selector = document.getElementById("feature-type");
+var features = document.getElementById("features");
+
+selector.onchange = function() {
+    readTextFile();
+}
+
+window.onload = function() {
+    readTextFile();
+}
+
+function readTextFile() {
+    features.innerHTML = "";
+    var rawFile = new XMLHttpRequest();
+    rawFile.open("GET", "../text/" + selector.value + ".txt", false);
+    rawFile.onreadystatechange = function () {
+        if(rawFile.readyState === 4)
+        {
+            var text = rawFile.responseText.split("/n");
+            for (var i = 0; i < text.length; i++) {
+                features.innerHTML = features.innerHTML + text[i] + "<br>";
+            }
+        }
+    }
+    rawFile.send(null);
+}
