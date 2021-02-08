3347978000664351514b9fad7dc698560278082a
DoctorProctor <45911516+1DoctorProctor1@users.noreply.github.com>
2021-02-08 05:02:48 +0100

    Roadmap Update

--- script/roadmap.js
+++ script/roadmap.js
@@ -1,7 +1,13 @@
-var selector = document.getElementById("feature-type");
-var features = document.getElementById("features-list");
+var soonselector = document.getElementById("soon-type");
+var soon = document.getElementById("soon-list");
+var featureselector = document.getElementById("feature-type");
+var feature = document.getElementById("features-list")
 
-selector.onchange = function() {
+soonselector.onchange = function() {
+    readTextFile();
+}
+
+featureselector.onchange = function() {
     readTextFile();
 }
 
@@ -10,17 +16,30 @@ window.onload = function() {
 }
 
 function readTextFile() {
-    features.innerHTML = "";
+    soon.innerHTML = "";
+    feature.innerHTML = "";
     var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", "../text/" + selector.value + ".txt", false);
+    var rawFile2 = new XMLHttpRequest();
+    rawFile.open("GET", "../text/soon/" + soonselector.value + ".txt", false);
+    rawFile2.open("GET", "../text/feature/" + featureselector.value + ".txt", false);
     rawFile.onreadystatechange = function () {
         if(rawFile.readyState === 4)
         {
             var text = rawFile.responseText.split("/n");
             for (var i = 0; i < text.length; i++) {
-                features.innerHTML = features.innerHTML + text[i] + "<br><br>";
+                soon.innerHTML = soon.innerHTML + (i+1) + "." + text[i] + "<br><br>";
+            }
+        }
+    }
+    rawFile2.onreadystatechange = function () {
+        if(rawFile2.readyState === 4)
+        {
+            var text = rawFile2.responseText.split("/n");
+            for (var i = 0; i < text.length; i++) {
+                feature.innerHTML = feature.innerHTML + (i+1) + "." + text[i] + "<br><br>";
             }
         }
     }
     rawFile.send(null);
+    rawFile2.send(null);
 }
