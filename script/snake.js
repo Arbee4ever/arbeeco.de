09e1c84a945c33603402653c3f43ba32986c87cb
DoctorProctor <45911516+1DoctorProctor1@users.noreply.github.com>
2021-02-19 18:00:33 +0100

    Update snake.js

--- script/snake.js
+++ script/snake.js
@@ -17,6 +17,8 @@ let food_x;
 let food_y;
 let dx = 10;
 let dy = 0;
+var xDown = null;                                                        
+var yDown = null;
 
 const snakeboard = document.getElementById("snakeboard");
 const snakeboard_ctx = snakeboard.getContext("2d");
@@ -26,6 +28,8 @@ main();
 gen_food();
 
 document.addEventListener("keydown", change_direction);
+document.addEventListener('touchstart', handleTouchStart, false);        
+document.addEventListener('touchmove', handleTouchMove, false);
 
 function main() {
     if(has_game_ended()) return;
@@ -138,3 +142,43 @@ function move_snake() {
         snake.pop();
     }
 }
+
+function getTouches(evt) {
+    return evt.touches ||             // browser API
+           evt.originalEvent.touches; // jQuery
+  }                                                     
+  
+  function handleTouchStart(evt) {
+      const firstTouch = getTouches(evt)[0];                                      
+      xDown = firstTouch.clientX;                                      
+      yDown = firstTouch.clientY;                                      
+  };                                                
+  
+  function handleTouchMove(evt) {
+      if ( ! xDown || ! yDown ) {
+          return;
+      }
+  
+      var xUp = evt.touches[0].clientX;                                    
+      var yUp = evt.touches[0].clientY;
+  
+      var xDiff = xDown - xUp;
+      var yDiff = yDown - yUp;
+  
+      if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
+          if ( xDiff > 0 ) {
+              /* left swipe */ 
+          } else {
+              /* right swipe */
+          }                       
+      } else {
+          if ( yDiff > 0 ) {
+              /* up swipe */ 
+          } else { 
+              /* down swipe */
+          }                                                                 
+      }
+      /* reset values */
+      xDown = null;
+      yDown = null;                                             
+  };
