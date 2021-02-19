44e5e50cbf1e5ca0a6c57a54a796d131b1418ed1
DoctorProctor <45911516+1DoctorProctor1@users.noreply.github.com>
2021-02-19 18:11:04 +0100

    Update snake.js

--- script/snake.js
+++ script/snake.js
@@ -144,41 +144,52 @@ function move_snake() {
 }
 
 function getTouches(evt) {
-    return evt.touches ||             // browser API
-           evt.originalEvent.touches; // jQuery
-  }                                                     
+    return evt.touches || evt.originalEvent.touches;
+}                                                     
   
-  function handleTouchStart(evt) {
-      const firstTouch = getTouches(evt)[0];                                      
-      xDown = firstTouch.clientX;                                      
-      yDown = firstTouch.clientY;                                      
-  };                                                
+function handleTouchStart(evt) {
+    const firstTouch = getTouches(evt)[0];                                      
+    xDown = firstTouch.clientX;                                      
+    yDown = firstTouch.clientY;                                      
+};                                                
   
-  function handleTouchMove(evt) {
-      if ( ! xDown || ! yDown ) {
-          return;
-      }
+function handleTouchMove(evt) {
+    if ( ! xDown || ! yDown ) {
+        return;
+    }
   
-      var xUp = evt.touches[0].clientX;                                    
-      var yUp = evt.touches[0].clientY;
+    var xUp = evt.touches[0].clientX;                                    
+    var yUp = evt.touches[0].clientY;
   
-      var xDiff = xDown - xUp;
-      var yDiff = yDown - yUp;
-  
-      if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {/*most significant*/
-          if ( xDiff > 0 ) {
-              /* left swipe */ 
-          } else {
-              /* right swipe */
-          }                       
-      } else {
-          if ( yDiff > 0 ) {
-              /* up swipe */ 
-          } else { 
-              /* down swipe */
-          }                                                                 
-      }
-      /* reset values */
-      xDown = null;
-      yDown = null;                                             
+    var xDiff = xDown - xUp;
+    var yDiff = yDown - yUp;
+
+    const goingUp = dy === -10;
+    const goingDown = dy === 10;
+    const goingRight = dx === 10;
+    const goingLeft = dx === -10;
+ 
+    if ( Math.abs( xDiff ) > Math.abs( yDiff ) ) {
+        if ( xDiff > 0 && !goingRight) {
+            dx = -10;
+            dy = 0;
+        } else {
+            if(!goingLeft) {
+                dx = 10;
+                dy = 0;
+            }
+        }                       
+    } else {
+        if ( yDiff > 0 && !goingDown) {
+            dx = 0;
+            dy = -10;
+        } else {
+            if(!goingUp) { 
+                dx = 0;
+                dy = 10;
+            }
+        }                                                                 
+    }
+    xDown = null;
+    yDown = null;                                             
   };
