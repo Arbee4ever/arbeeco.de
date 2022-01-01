6fbc8eb340d5446d960dc25b464e41c368334953
Arbee4ever <robin.baenecke@mail.de>
2022-01-01 02:24:56 +0100

    Made mainbutton changing transparency smoother

--- script/index.js
+++ script/index.js
@@ -15,51 +15,43 @@ function showContact() {
 		var id = null;
 		switch (state) {
 			case 1:
-				mainbutton.style.zIndex = -1;
-				id = setInterval(() => {
-					if(index <= 1) {
-						state = 0.5;
-						index = index + 0.01;
-						if(1-index >= 0.2) {
-							mainbutton.style.opacity = 1-index;
-						}
-						description.style.opacity = 1-index;
-						for (let x = 0; x < button.length; x++) {
-							const element = button[x];
-							element.style.opacity = index;
-							element.style.zIndex = 1;
-						}
-					} else {
-						mainbutton.style.animationPlayState = "paused";
-						state = 0;
-						clearInterval(id);
+				for(i=0; i<10; i++) {
+					state = 0.5;
+					index = (i/10);
+					mainbutton.style.opacity = 1-index;
+					description.style.opacity = 0;
+					for (let x = 0; x < button.length; x++) {
+						const element = button[x];
+						element.style.opacity = index;
+						element.style.zIndex = 1;
 					}
-				}, 1)
+				}
+
+				mainbutton.style.animationPlayState = "paused";
+				mainbutton.style.zIndex = -1;
+				state = 0;
 				break;
 			
 			case 0:
-				id = setInterval(() => {
-					if(index >= 0) {
-						state = 0.5;
-						index = index - 0.01;
-						mainbutton.style.opacity = 1-index;
-						description.style.opacity = 1-index;
-						for (let x = 0; x < button.length; x++) {
-							const element = button[x];
-							element.style.opacity = index;	
-							element.style.zIndex = -1;
-						}
-					} else {
-						mainbutton.style.animationPlayState = "running";
-						mainbutton.style.zIndex = 1;
-						state = 1;
-						clearInterval(id);
+				for(i=10; i>0; i--) {
+					state = 0.5;
+					index = (i/10);
+					mainbutton.style.opacity = 1-index;
+					description.style.opacity = 1;
+					for (let x = 0; x < button.length; x++) {
+						const element = button[x];
+						element.style.opacity = index;
+						element.style.zIndex = -1;
 					}
-				}, 1)
+				}
+
+				mainbutton.style.animationPlayState = "running";
+				mainbutton.style.zIndex = 1;
+				state = 1;
 				break;
 
 			default:
-				alert("Something went wrong! Please report this to Arbee!")
+				alert("Something went wrong! Please report this to Arbee!");
 				break;
 		}
 	}
@@ -73,7 +65,7 @@ function showContact() {
 let scroll = 0;
 document.addEventListener("wheel", function (e) {
 	scroll = (scroll - e.deltaY/10)%360;
-	root.style.setProperty("--animation-rotation", 
+	root.style.setProperty("--animation-rotation",
 	if(state == 0) {
 		showContact();
 	}
