488a13ca2536bec6899855a4076606d56a457d80
Arbee4ever <robin.baenecke@mail.de>
2022-01-02 20:09:38 +0100

    Updated Icons

--- script/index.js
+++ script/index.js
@@ -11,45 +11,45 @@ var index = 0;
 var state = 1;
 
 function showContact() {
-	if (state == 0 || state == 1) {
-		var id = null;
-		switch (state) {
-			case 1:
-				mainbutton.style.opacity = 0.1;
-				for (let x = 0; x < button.length; x++) {
-					const element = button[x];
-					element.style.opacity = 1;
-					element.style.zIndex = 1;
-				}
+    if (state == 0 || state == 1) {
+        var id = null;
+        switch (state) {
+            case 1:
+                mainbutton.style.opacity = 0.1;
+                for (let x = 0; x < button.length; x++) {
+                    const element = button[x];
+                    element.style.opacity = 1;
+                    element.style.zIndex = 1;
+                }
 
-				description.style.opacity = 0;
-				mainbutton.style.animationPlayState = "paused";
-				mainbutton.style.zIndex = -1;
-				state = 0;
-				break;
+                description.style.opacity = 0;
+                mainbutton.style.animationPlayState = "paused";
+                mainbutton.style.zIndex = -1;
+                state = 0;
+                break;
 
-			case 0:
-				mainbutton.style.opacity = 1;
-				for (let x = 0; x < button.length; x++) {
-					const element = button[x];
-					element.style.opacity = 0;
-					element.style.zIndex = -1;
-				}
+            case 0:
+                mainbutton.style.opacity = 1;
+                for (let x = 0; x < button.length; x++) {
+                    const element = button[x];
+                    element.style.opacity = 0;
+                    element.style.zIndex = -1;
+                }
 
-				description.style.opacity = 1;
-				mainbutton.style.animationPlayState = "running";
-				mainbutton.style.zIndex = 1;
-				state = 1;
-				break;
+                description.style.opacity = 1;
+                mainbutton.style.animationPlayState = "running";
+                mainbutton.style.zIndex = 1;
+                state = 1;
+                break;
 
-			default:
-				alert("Something went wrong! Please report this to Arbee!");
-				break;
-		}
-	}
+            default:
+                alert("Something went wrong! Please report this to Arbee!");
+                break;
+        }
+    }
 }
 
 function slide() {
-	main.classList.toggle("hide");
-	projects.classList.toggle("hide");
+    main.classList.toggle("hide");
+    projects.classList.toggle("hide");
 }
