7ec020d8691ae17f8f98a77cf18fb86f836738c3
Arbee4ever <robin.baenecke@mail.de>
2022-01-14 21:37:40 +0100

    merged test files into index files

--- script/index.js
+++ script/index.js
@@ -1,43 +1,48 @@
-const mainbutton = document.getElementById("mainbutton");
-const description = document.getElementById("description");
 const button = document.getElementsByClassName("button");
+const center = document.getElementById("center");
+const centerIcon2 = document.getElementById("centerIcon2");
+const centerIcon = document.getElementById("centerIcon");
+const vignette = document.getElementById("vignette");
 var state = 1;
 
-function showContact() {
-    if (state == 0 || state == 1) {
-        var id = null;
-        switch (state) {
-            case 1:
-                mainbutton.style.opacity = 0.1;
-                for (let x = 0; x < button.length; x++) {
-                    const element = button[x];
-                    element.style.opacity = 1;
-                    element.style.zIndex = 1;
-                }
+centerIcon.onclick = function () { toggleHide() };
+centerIcon2.onclick = function () { toggleHide() };
 
-                description.style.opacity = 0;
-                mainbutton.style.animationPlayState = "paused";
-                mainbutton.style.zIndex = -1;
-                state = 0;
-                break;
-
-            case 0:
-                mainbutton.style.opacity = 1;
-                for (let x = 0; x < button.length; x++) {
-                    const element = button[x];
-                    element.style.opacity = 0;
-                    element.style.zIndex = -1;
-                }
-
-                description.style.opacity = 1;
-                mainbutton.style.animationPlayState = "running";
-                mainbutton.style.zIndex = 1;
-                state = 1;
-                break;
-
-            default:
-                alert("Something went wrong! Please report this to Arbee!");
-                break;
+function toggleHide() {
+    if (state == 1) {
+        for (let x = 0; x < button.length; x++) {
+            const element = button[x];
+            element.classList.toggle("hide");
+            element.style.zIndex = 1;
         }
+        state = 0;
+    } else {
+        for (let x = 0; x < button.length; x++) {
+            const element = button[x];
+            element.classList.toggle("hide");
+            element.style.zIndex = -1;
+        }
+        state = 1;
+    }
+    centerIcon2.classList.toggle("hide");
+    centerIcon.classList.toggle("hide");
+}
+
+center.onmouseenter = function () {
+    if (centerIcon2.classList.contains("hide")) {
+        center.classList.toggle("bigger");
+    }
+}
+
+center.onmouseleave = function () {
+    if (centerIcon2.classList.contains("hide")) {
+        center.classList.toggle("bigger");
+    }
+}
+
+vignette.onclick = function () {
+    if (centerIcon.classList.contains("hide")) {
+        toggleHide();
+        center.classList.toggle("bigger");
     }
 }
