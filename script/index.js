1cd53fe74b4d089a9cbf57da67739d5b19b53075
Arbee4ever <robin.baenecke@mail.de>
2022-01-18 03:28:17 +0100

    Update

--- script/index.js
+++ script/index.js
@@ -52,8 +52,8 @@ window.onscroll = function() {scrollFunction()};
 
 function scrollFunction() {
   if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
+    topBtn.style.display = "block";
   } else {
-    mybutton.style.display = "none";
+    topBtn.style.display = "none";
   }
 }
