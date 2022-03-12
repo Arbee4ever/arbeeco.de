a408bb5663059b0788cf2fb2b3f55130005f6828
Arbee4ever <robin.baenecke@mail.de>
2022-03-12 18:27:57 +0100

    topBtn changes

--- script/top.js
+++ script/top.js
@@ -1,7 +1,12 @@
 window.onscroll = function() {
-    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-        topBtn.style.display = "block";
-    } else {
-        topBtn.style.display = "none";
-    }
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    topBtn.style.display = "block";
+  } else {
+    topBtn.style.display = "none";
+  }
+}
+
+topBtn.onclick = function() {
+  document.body.scrollTop = 0
+  document.documentElement.scrollTop = 0;
 }
