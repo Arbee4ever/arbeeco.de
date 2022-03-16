add47273f95f011101e18136a387215cc815df69
Arbee4ever <robin.baenecke@mail.de>
2022-03-17 00:54:08 +0100

    Filter by link added

---
+++ script/filter.js
@@ -0,0 +1,13 @@
+const post = document.getElementsByClassName("post");
+const postCategory = document.getElementsByClassName("postCategory");
+
+for (let i = 0; i < postCategory.length; i++) {
+    const element = postCategory[i];
+    const queryString = window.location.search;
+    const urlParams = new URLSearchParams(queryString);
+    if (urlParams.has("c") && element.firstChild.textContent != urlParams.get("c")) {
+        element.parentNode.style.display = "none";
+    } else {
+        element.parentNode.style.display = "block";
+    }
+}
