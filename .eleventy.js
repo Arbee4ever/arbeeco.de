c6d6c5c17d355feaacd84a68109b87891c6b153e
Arbee4ever <robin.baenecke@mail.de>
2022-08-08 02:58:13 +0200

    postPreview testing

--- .eleventy.js
+++ .eleventy.js
@@ -1,17 +1,27 @@
-module.exports = function(eleventyConfig) {
-  eleventyConfig.addPassthroughCopy('CNAME');
-  eleventyConfig.addPassthroughCopy("./authorimg");
-  eleventyConfig.addPassthroughCopy("./css");
-  eleventyConfig.addPassthroughCopy("./img");
-  eleventyConfig.addPassthroughCopy("./links");
-  eleventyConfig.addPassthroughCopy("./postimg");
-  eleventyConfig.addPassthroughCopy("./script");
+const striptags = require("striptags");
+const he = require("he");
+
+const WHITESPACE = /\s+/;
+
+module.exports = function (eleventyConfig) {
+    eleventyConfig.addPassthroughCopy('CNAME');
+    eleventyConfig.addPassthroughCopy("./authorimg");
+    eleventyConfig.addPassthroughCopy("./css");
+    eleventyConfig.addPassthroughCopy("./img");
+    eleventyConfig.addPassthroughCopy("./links");
+    eleventyConfig.addPassthroughCopy("./postimg");
+    eleventyConfig.addPassthroughCopy("./script");
+
+    eleventyConfig.addFilter("fifty_words", (content) => {
+        const plainText = he.decode(striptags(content), { strict: true });
+        return plainText.split(WHITESPACE).slice(0, 50).join(" ");
+    });
 
     return {
-      dir: {
-        input: "./",
-        output: "./_site",
-        layouts: "./_layouts"
-      }
+        dir: {
+            input: "./",
+            output: "./_site",
+            layouts: "./_layouts"
+        }
     };
-  }
+}
