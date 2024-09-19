44577a56cf50f72d8f0d4509771067f4e1ff1fc5
Arbee4ever <arbee@arbeeco.de>
2024-09-19 22:55:27 +0200

    fix: add proper favicons

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -12,7 +12,6 @@
 	<meta name="description"
 				content="I (ARBEE) am a german guy who likes programming fun things. I'm creating Minecraft mods and Websites with Svelte.">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
-	<link rel="icon" href={ArbeeLogo}>
 	<link rel="canonical" href="https://arbeeco.de">
 	<meta name="twitter:card" content="summary" />
 	<meta name="twitter:site" content="@ARBEE_codes" />
@@ -25,6 +24,16 @@
 				content="https://arbeeco.de/authorImg/2022-01_ARBEE_Bild-Marke_rgb_weiss_bg_verlauf_01.png" />
 	<meta name="theme-color" content="#2b8eaf" />
 	<meta name="keywords" content="Blog, Modding, Gamedev, Arbee, GitHub">
+
+	<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+	<link rel="manifest" href="/site.webmanifest">
+	<link rel="mask-icon" href="/safari-pinned-tab.svg" color="#5bbad5">
+	<meta name="msapplication-TileColor" content="#da532c">
+	<meta name="theme-color" content="#ffffff">
+
+
 </svelte:head>
 
 <Header></Header>
