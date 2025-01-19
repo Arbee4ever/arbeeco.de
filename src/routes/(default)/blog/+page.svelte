8748e16dba5b945c4bd282f9a69ab921b9d34612
Arbee4ever <arbee@arbeeco.de>
2025-01-19 16:10:46 +0100

    feat: filter by category functionality

--- src/routes/(default)/blog/+page.svelte
+++ src/routes/(default)/blog/+page.svelte
@@ -1,9 +1,18 @@
 <script lang="ts">
 	import type { PageData } from './$types';
 	import BlogSection from '$lib/components/BlogSection.svelte';
+	import { page } from '$app/stores';
 
 	export let data: PageData;
 	let posts = data.posts;
+
+	if ($page.url.searchParams.has("c")) {
+		let category = $page.url.searchParams.get("c")
+		posts = posts.filter(post => {
+			console.log('post', post);
+			return post.category.key == category;
+		});
+	}
 </script>
 
 <BlogSection {posts} all={true}></BlogSection>
