e148bc75003ef3d8f50eed259137725012501f55
Arbee4ever <arbee@arbeeco.de>
2025-01-19 14:22:00 +0100

    fix: style images loaded from external source

--- src/lib/components/blog/ModBody.svelte
+++ src/lib/components/blog/ModBody.svelte
@@ -12,8 +12,19 @@
 	}
 </script>
 
-<div>
+<div class="mod-body">
 	{#await loadModData(data.slug) then mod}
 		{@html marked(mod.body)}
 	{/await}
 </div>
+
+<style lang="scss">
+	.mod-body {
+		:global img {
+			width: fit-content;
+			max-width: 100%;
+			border-radius: 1vw;
+			box-shadow: 0 0 32px 0 rgba(0, 0, 0, 0.37);
+		}
+	}
+</style>
