a51dd6064000e9baefcd1b2761631e734d78b2fa
arbee4ever <arbee@arbeeco.de>
2026-07-12 08:55:41 +0200

    fix: url styling for markdown links

--- src/lib/components/blog/Markdown.svelte
+++ src/lib/components/blog/Markdown.svelte
@@ -46,3 +46,15 @@
 <div bind:this={markdownBody} class="markdown-body">
 	{@html marked(data.markdown)}
 </div>
+
+<style lang="scss">
+	:global .markdown-body {
+		p > a {
+			color: #00BFFF;
+
+			&:hover {
+				text-shadow: 0 0 20px #00BFFF;
+			}
+		}
+	}
+</style>
